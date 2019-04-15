0eccc7cf89b1dcf37a4b811d8a19a88cf474b392
wowczuk1996 <k96.wowczuk@gmail.com>
2019-04-15 22:26:03 +0200

    add

---
+++ src/components/map/mapFlight.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+class Map extends Component {
+    render() {
+        return (
+            <Map google={this.props.google} zoom={14}>
+
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        {/*<h1>{this.state.selectedPlace.name}</h1>*/}
+                    </div>
+                </InfoWindow>
+            </Map>
+
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+})(Map)
